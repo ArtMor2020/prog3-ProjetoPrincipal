7acb18cc57975c211747fe89097f874ac537dd93
ArtMor2020 <arthurmoro8@gmail.com>
2025-05-13 21:27:06 -0300

    part of Migrations added

---
+++ ProjectForum/app/Database/Migrations/2025-05-13-000008_Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Post extends Migration
+{
+    public function up()
+    {
+        // post
+        $this->forge->addField([
+            'id'           => ['type' => 'INT', 'auto_increment' => true],
+            'id_user'      => ['type' => 'INT'],
+            'id_community' => ['type' => 'INT'],
+            'title'        => ['type' => 'VARCHAR', 'constraint' => 255],
+            'description'  => ['type' => 'TEXT'],
+            'posted_at'    => ['type' => 'TIMESTAMP'],
+            'updated_at'   => ['type' => 'TIMESTAMP', 'null' => true],
+            'is_approved'  => ['type' => 'BOOLEAN'],
+            'is_deleted'   => ['type' => 'BOOLEAN'],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('id_user', 'user', 'id');
+        $this->forge->addForeignKey('id_community', 'community', 'id');
+        $this->forge->createTable('post');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('post');
+    }
+}
