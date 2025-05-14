57ef6ee41dc7f26107e61aca83bdef092d131afa
Improvez <lucaszanotto@hotmail.com>
2025-05-13 21:50:37 -0300

    add: as 15 migrations

---
+++ ProjectForum/app/Database/Migrations/2025-05-13-0000010_Post_View.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PostView extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_post' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'viewed_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+
+        // Foreign Keys
+        $this->forge->addForeignKey('id_post', 'post', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('id_user', 'user', 'id', 'CASCADE', 'CASCADE');
+        
+        $this->forge->createTable('post_view');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('post_view');
+    }
+}
