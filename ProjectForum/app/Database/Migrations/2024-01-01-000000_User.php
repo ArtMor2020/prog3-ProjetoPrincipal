01f3dc1072b1f4d5d82bf3917c85308f545f5337
Improvez <lucaszanotto@hotmail.com>
2025-05-13 21:30:14 -0300

    add: migrations

---
+++ ProjectForum/app/Database/Migrations/2024-01-01-000000_User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => ['type' => 'INT', 'auto_increment' => true],
+            'name'        => ['type' => 'VARCHAR', 'constraint' => 255],
+            'email'       => ['type' => 'VARCHAR', 'constraint' => 255],
+            'password'    => ['type' => 'VARCHAR', 'constraint' => 255],
+            'about'       => ['type' => 'TEXT'],
+            'is_private'  => ['type' => 'BOOLEAN'],
+            'is_banned'   => ['type' => 'BOOLEAN'],
+            'is_deleted'  => ['type' => 'BOOLEAN'],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user');
+    }
+}
