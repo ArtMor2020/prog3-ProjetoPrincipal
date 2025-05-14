e4343803a27c15e63e89d0d6d0f4533433f396b9
ArtMor2020 <arthurmoro8@gmail.com>
2025-05-13 21:47:31 -0300

    add migration

---
+++ ProjectForum/app/Database/Migrations/2025-05-13-0000012_Attachment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Attachment extends Migration
+{
+    public function up()
+    {
+        // attachment
+        $this->forge->addField([
+            'id'         => ['type' => 'INT', 'auto_increment' => true],
+            'type'       => ['type' => 'ENUM', 'constraint' => ['IMAGE','VIDEO','GIF','DOCUMENT','ZIP','OTHER']],
+            'path'       => ['type' => 'TEXT'],
+            'is_deleted' => ['type' => 'BOOLEAN'],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('attachment');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('attachment');
+    }
+}
